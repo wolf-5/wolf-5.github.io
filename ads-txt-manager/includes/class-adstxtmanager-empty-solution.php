7bcdde1d174b4cb6e21c291ee4ce6605a6f69000
THE WOLF HACK <70449764+wolf-5@users.noreply.github.com>
2023-06-23 10:00:59 +0300

    wolf-5

--- ads-txt-manager/includes/class-adstxtmanager-empty-solution.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class AdsTxtManager_Empty_Solution implements iAdsTxtManager_Solution {
-    public function SetupSolution(){ return; }
-    public function TearDownSolution(){ return; }
-}
