525ad376fb7c6482b8ccddbf1241fde0d2484d1d
THE WOLF HACK <thewolfgames@yahoo.com>
2023-11-03 08:59:57 +0300

    wolf-5

---
+++ ps5/payload_map.js
@@ -0,0 +1,117 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'HEN',
+            description: 'FPKG enabler',
+            fileName: 'ps5-kstuff-v1.2.bin',
+            author: 'sleirsgoevy',
+            source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
+            version: '1.1'
+        },
+        {
+            displayTitle: 'etaHEN',
+            description: 'etaHEN 1.1b by LM',
+            fileName: 'etaHEN-1.1b.bin',
+            author: 'LM',
+            source:'https://github.com/LightningMods/etaHEN',
+            version: '?'
+        },
+        {
+            displayTitle: 'FTP PS5 R/W (P/S)',
+            description: 'FTP Server for PS5',
+            fileName: 'ftps5-p.elf',
+            author: 'SiSTR0, zecoxao, EchoStretch',
+            source:'https://github.com/EchoStretch/FTPS5/releases',
+            version: '1.4'
+        },
+        {
+            displayTitle: 'etaHEN WITH CHEATS',
+            description: 'etaHEN  1.1b wit hcheats by LM',
+            fileName: 'etaHENwithcheats-1.1b.bin',
+            author: 'LM',
+            source:'https://github.com/LightningMods/etaHEN',
+            version: '?'
+        },
+        {
+            displayTitle: 'LIBHIJACKER GAME-PACTH',
+            description: '(v1.124) Patches supported games .',
+            fileName: 'libhijacker-spawner-1.124.elf;libhijacker-daemon-1.124.elf',
+            author: 'illusion0001, astrelsky',
+            source: 'https://github.com/illusion0001/libhijacker/releases',
+            loader: 'libhijacker',
+            version: '1.124'
+        },
+        {
+            displayTitle: 'SYS INFO',
+            description: 'temperature information, frequency.',
+            fileName: 'hwinfo-tornblom.elf',
+            author: '?',
+            source:'?',
+            version: '?'
+        },
+        {
+            displayTitle: 'KLOG SERVER',
+            description: 'KLOG SERVER',
+            fileName: 'KLOG SERVER.bin',
+            author: '?',
+            source:'?',
+            version: '?'
+        },
+
+        {
+            displayTitle: 'VERSION',
+            description: 'Shows kernel build, os and sdk versions',
+            fileName: 'versions.elf',
+            author: '?',
+            source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'FAN CONTROL',
+            description: 'fan threshold set 60° PS5',
+            fileName: 'fan_threshold.elf',
+            author: '?',
+            source:'?',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'GET OS VERSION',
+            description: 'Shows different information parameters',
+            fileName: 'getOsVersion.bin',
+            author: 'todo',
+            source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'REVERSE THE FW 4.03/ 4.50 / 99.99',
+            description: 'Round trip 4.03 / 4.50 / 99.99 by Jaafar',
+            fileName: 'TO_4.50_4.03_99.99.elf',
+            author: '?',
+            source:'?',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'PS5 BROWSER APP CACHE REMOVE',
+            description: 'Removes appcache from browser.',
+            fileName: 'Browser_appCache_remove.elf',
+            author: 'Storm21CH',
+            source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
+            version: '1.0fix'
+        },
+        {
+            displayTitle: 'REVERSE THE FW 4.03/ 4.50 / 1.00',
+            description: 'Return to FW 1.00 to reinstall the FW by Jaafar',
+            fileName: 'TO_4.50_4.03_1.00.elf',
+            author: '?',
+            source:'?',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'PAYLOAD ELF LOADER',
+            description: 'Uses port 9021. Persistent network elf loader',
+            fileName: 'elfldr.elf',
+            author: 'john-tornblom',
+            source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
+            version: '0.5'
+        },
+    ];
