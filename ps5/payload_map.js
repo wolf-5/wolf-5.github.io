932c11cf09e96900912a9b56e7ce6aed62e6f863
THE WOLF HACK <70449764+wolf-5@users.noreply.github.com>
2023-12-25 15:49:34 +0300

    wolf-5

--- ps5/payload_map.js
+++ ps5/payload_map.js
@@ -3,7 +3,7 @@ const payload_map =
 	        {
             displayTitle: 'etaHEN',
             description: 'etaHEN 1.2b by LM',
-            fileName: 'etaHEN_1.2B',
+            fileName: 'etaHEN_1.2B.bin',
             author: 'LM',
             source:'https://github.com/LightningMods/etaHEN',
             version: '1.2B'
