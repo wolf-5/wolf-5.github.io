07a2da2e29c80b725b17b1d4ea400cf2fd7eb9eb
THE WOLF HACK <70449764+wolf-5@users.noreply.github.com>
2025-02-15 22:37:06 +0300

    test

--- test/README.md
+++ test/README.md
@@ -1,11 +1,10 @@
-PS5 HOST JAILBREAK 4.03 4.50 4.51 etaHEN AND MORE MAYLOADS (not PSFREE) WITH CACHE OFFLINE
+PS5 HOST LITE JAILBREAK 3.xx- 4.03 4.50 4.51 AUTO etaHEN 2.0b (not PSFREE)
 
-PKG PS5 --
+https://ps5xploit.github.io/lite/
 
-GAMES: https://www.mediafire.com/file/of7892c7cwp05hq/PS5XPLOIT-HOST-OFFLINE.pkg/file
+offline cache / GAME
+PKG: https://www.mediafire.com/file/08unc7417u0lazc/PS5-HOST-LITE-OFFLINE.pkg/file
 
-MEDIA: https://www.mediafire.com/file/nmmiwvo4gg24tm4/mPS5XPLOIT-HOST-OFFLINE.pkg/file
 
---
-
-URL: https://ps5xploit.github.io/cacheoff
+offline cache / MEDIA
+https://www.mediafire.com/file/2yxgg1o687r6wwo/mPS5-HOST-LITE-OFFLINE.pkg/file
--- test/payload_map.js
+++ test/payload_map.js
@@ -1,95 +1,19 @@
 const payload_map = [
-  {
-    displayTitle: 'etaHEN',
-    description: '', // Dejar "description" vacío
-    info: 'Descripción de etaHEN', // Mantener "info"
-    fileName: 'etaHEN-20B.bin',
-    author: 'LM',
-    version: '?'
-  },
-  {
-    displayTitle: 'etaHEN 1.8b',
-    description: '', // Dejar "description" vacío
-    info: 'Descripción de etaHEN', // Mantener "info"
-    fileName: 'etaHEN-18B.bin',
-    author: 'LM',
-    version: '1.8b'
-  },
- 
-  {
-    displayTitle: 'HW Info',
-    description: '', // Dejar "description" vacío
-    info: 'Descripción de HW Info', // Mantener "info"
-    fileName: 'hwinfo-tornblom.elf',
-    author: '?',
-    source: '?',
-    version: '?'
-  },
-  {
-    displayTitle: 'Remove Cache',
-    description: '', // Dejar "description" vacío
-    info: 'Descripción de Remove Cache', // Mantener "info"
-    fileName: 'Browser_appCache_remove.elf',
-    author: 'Storm21CH',
-    version: '1.0fix'
-  },
-  {
-    displayTitle: 'Version',
-    description: '', // Dejar "description" vacío
-    info: 'Descripción de Versions', // Mantener "info"
-    fileName: 'versions.elf',
-    author: '?',
-    version: '1.0'
-  },
-  {
-    displayTitle: 'Fan control',
-    description: '', // Dejar "description" vacío
-    info: 'Descripción de Fan control', // Mantener "info"
-    fileName: 'fan_threshold.elf',
-    author: '?',
-    source: '?',
-    version: '1.0'
-  },
-   {
-            displayTitle: 'Backup DB',
-            description: '', // Dejar "description" vacío
-            fileName: 'BackupDB.elf',
-            info: 'Make a BACKUP from db to usb', // Mantener "info"
-            author: 'Make a BACKUP from db to usb',
-            source: 'Make a BACKUP from db to usb',
-            version: '1.1'
-        },
-  
-{
-            displayTitle: 'ELF Loader',
-            description: '', // Dejar "description" vacío
-            fileName: 'elfldr.elf',
-            info: 'ELF Loader', // Mantener "info"
-            author: 'ELF Loader',
-            source: 'ELF Loader',
-            version: '1.1'
-        },
-// {
-    //     displayTitle: 'ps5-welcome',
-    //     description: '',
-    //     info: 'ps5-welcome',
-    //     fileName: 'ps5-welcome.elf',
-    //     author: 'mour0ne',
-    //     version: '?'
-    // },
+    {
+        displayTitle: 'ps5-debug',
+        description: 'ps5-debug ',
+        fileName: '1-ps5debug_10b2.elf',
+        author: 'goldhen',
+        source: 'n',
+        version: '1.02'
+    },
+    {
+        displayTitle: 'etaHEN',
+        description: 'FPKG enabler ',
+        fileName: 'etaHEN-20B.bin',
+        author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+        source: 'https://github.com/LightningMods/etaHEN',
+        version: '1.8 beta'
+    },
+    // Otros payloads si es necesario
 ];
-
-// JavaScript para mostrar info en lugar de description
-const btns = document.querySelectorAll('.btn');
-const infoElement = document.getElementById('info'); // Obtén el elemento de info por su ID
-
-btns.forEach(btn => {
-  btn.addEventListener('mouseover', () => {
-    const info = btn.getAttribute('data-info');
-    infoElement.textContent = info; // Actualiza el contenido del elemento de info
-  });
-
-  btn.addEventListener('mouseout', () => {
-    infoElement.textContent = ''; // Limpia el contenido cuando el ratón sale del elemento
-  });
-});
